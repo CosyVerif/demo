5f8fc2b629bce4e9e08ae4f7cd2f963c348ec4e4
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-10-02 23:45:40 +0200

    Update P/T net simulator (not tested).

--- src/simulator.lua
+++ src/simulator.lua
@@ -1,177 +1,124 @@
 #! /usr/bin/env lua
 
-local cli      = require "cliargs"
-local connect  = require "cosy.connexion.ev"
-local observed = require "cosy.lang.view.observed"
+local global = _ENV or _G
+global.ev = true
 
-local type = require "cosy.util.type"
-local raw  = require "cosy.lang.data" . raw
-local map  = require "cosy.lang.iterators" . map
-local set  = require "cosy.lang.iterators" . set
+local Cosy      = require "cosy"
+local Data      = require "cosy.data"
+local Helper    = require "cosy.helper"
+local _         = require "cosy.util.string"
+local cli       = require "cliargs"
+local logging   = require "logging"
+logging.console = require "logging.console"
+local logger    = logging.console "%level %message\n"
 
-cli:set_name ("simulator.lua")
-cli:add_option (
-  "-u, --url=<URL>",
-  "editor URL",
-  "ws://localhost:8080"
-)
-cli:add_argument (
+cli:set_name ("pt.lua")
+cli:add_argument(
   "resource",
   "resource to edit"
 )
-cli:add_argument (
-  "token",
-  "user token"
+cli:add_option (
+  "--username=<string>",
+  "username"
 )
-
+cli:add_option (
+  "--password=<string>",
+  "password"
+)
+cli:add_option (
+  "--editor=<URL>",
+  "editor URL",
+  "ws://edit.cosyverif.io:8080"
+)
+cli:add_flag (
+  "-v, --verbose",
+  "enable verbose mode"
+)
 local args = cli:parse_args ()
 if not args then
   cli:print_help()
   return
 end
-local url       = args.url
-local token     = args.token
-local resource  = args.resource
 
-local model = connect {
-  editor   = url,
-  resource = resource,
-  token    = token,
-}
+local editor       = args.editor
+local resource     = args.resource
+local username     = args.username
+local password     = args.password
+local verbose_mode = args.verbose
 
-local function elements (e)
-  local function f (e, seen)
-    local result = {}
-    seen = seen or {}
-    if seen [e] then
-      return result
-    end
-    seen [e] = true
-    for k, x in map (e) do
-      if type (k) . tag and not k.persistent then
-        -- nothing
-      elseif type (x) . table then
-        if x.type then
-          result [raw (x)] = true
-        end
-        for y in set (f (x, seen)) do
-          result [y] = true
-        end
-      end
-    end
-    return result
-  end
-
-  local result = {}
-  for x in set (f (e)) do
-    result [observed (x)] = true
-  end
-  return result
+if verbose_mode then
+  logger:setLevel (logging.DEBUG)
+else
+  logger:setLevel (logging.INFO)
 end
 
--- Highlights fireable transitions:
-local function highlight ()
-  local number = 0
-  local nodes = elements (model)
-  for transition in set (nodes) do
-    if transition.type == "transition" then
-      local fireable = true
-      for arc in set (nodes) do
-        if arc.type == "arc" and
-           raw (arc.target) == raw (transition) and
-           not arc.source.marking then
-          fireable = false
+Helper.configure_editor (editor)
+Helper.configure_server ("", {
+  username = username,
+  password = password,
+})
+
+local function highlight (model)
+  local count = 0
+  for _, transition in pairs (model) do
+    if Helper.is_transition (transition) then
+      local all_pre = true
+      for _, arc in pairs (model) do
+        if  Helper.is_arc (arc)
+        and Helper.target (arc) == transition
+        and not Helper.source (arc).token () then
+          all_pre = false
+          break
         end
       end
-      if fireable ~= transition.highlighted then
-        transition.highlighted = fireable
-      end
-      print ("Transition " .. transition.name .. " can be fired? " .. tostring (transition.highlighted))
-      if fireable then
-        number = number + 1
+      if all_pre then
+        Helper.highlight (transition)
+        count = count + 1
       end
     end
   end
-  if number == 0 then
-    model.form.simulator.message = "Congratulations, you have reached a deadlock!"
-  end
-end
-
-local function clean ()
-  local nodes = elements (model)
-  for transition in set (nodes) do
-    transition.highlighted = false
+  if count == 0 then
+    Cosy.stop ()
   end
 end
 
 -- Performs fire:
 local function fire (transition)
-  print ("Firing transition " .. transition.name)
-  local o = observed [#observed]
-  observed [#observed] = nil
-  local nodes = elements (model)
-  for arc in set (nodes) do
-    if arc.type == "arc" and
-       raw (arc.target) == raw (transition) then
-       arc.source.marking = false
-     elseif arc.type == "arc" and
-       raw (arc.source) == raw (transition) then
-       arc.target.marking = true
+  local model = transition / 2
+  local all_pre = true
+  for _, arc in pairs (model) do
+    if  Helper.is_arc (arc)
+    and Helper.target (arc) == transition
+    and not Helper.source (arc).token () then
+      all_pre = false
+      break
     end
   end
-  for place in set (nodes) do
-    if place.type == "place" then
-      print ("Place: " .. place.name .. " = " .. tostring (place.marking))
+  if all_pre then
+    for _, arc in pairs (model) do
+      if  Helper.is_arc (arc)
+      and Helper.target (arc) == transition
+      and Helper.source (arc).token () then
+        Helper.source (arc).token = false
+      elseif  Helper.is_arc (arc)
+      and Helper.source (arc) == transition then
+        Helper.target (arc).token = true
+      end
     end
   end
-  observed [#observed] = o
-  transition.selected = false
+  Helper.deselect (transition)
 end
 
-
-model [cosy.tags.WS] . execute (function ()
-  model.form = {
-    simulator = {
-      name = "Simulator",
-      type = "form",
-      message = {
-        type  = "text",
-        name  = "Message",
-        value = "",
-        hint  = "",
-      },
-      close = {
-        type = "button",
-        name = "Close",
-        clicked   = false,
-        is_active = true,
-      },
-    },
-  }
-  highlight ()
-  observed [#observed + 1] = function (data, key)
-    coroutine.yield ()
-    if raw (data) == raw (model.form.simulator.close) and
-       key == "clicked" then
-      clean ()
-      model.form.simulator = nil
-      model [cosy.tags.WS] . stop ()
-    elseif key == "message" then
-    elseif data.type == "transition" and
-      key == "highlighted" then
-    elseif data.type == "transition" and
-      key == "selected" and
-      data [key] then
-      model [cosy.tags.WS] . execute (function ()
-        fire (data)
-        highlight ()
-      end)
-    else
-      model [cosy.tags.WS] . execute (function ()
-        highlight ()
-      end)
+function Cosy.main ()
+  local model = Helper.resource (resource)
+  highlight (model)
+  Data.on_write.simulator = function (target)
+    if Helper.is_transition (target)
+    and Helper.is_selected (target) then
+      fire (target)
+      highlight (model)
     end
   end
-end)
+end
 
-model [cosy.tags.WS] . loop ()
+Cosy.start ()
